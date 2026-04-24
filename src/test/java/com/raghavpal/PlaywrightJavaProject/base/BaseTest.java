41fb1af0f020d3b24393a2dd2f62d50b2f3cadae
Alex Silva <alexsantossilva44@gmail.com>
2026-04-24 22:35:20 +0100

    Initial commit: Playwright Java Project de Testes com Playwright

---
+++ src/test/java/com/raghavpal/PlaywrightJavaProject/base/BaseTest.java
@@ -0,0 +1,33 @@
+package com.raghavpal.PlaywrightJavaProject.base;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+/**
+ * Autor: Alex Silva
+ * Data: 24/04/2026
+ */
+
+public class BaseTest {
+
+    protected Playwright playwright;
+    protected Browser browser;
+    protected Page page;
+
+    @BeforeMethod
+    public void setUp() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(1000));
+        page = browser.newPage();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (browser != null) browser.close();
+        if (playwright != null) playwright.close();
+    }
+}
