528e0251089bbd84f36cb09d391ee10db4bcbb87
Alex Silva <alexsantossilva44@gmail.com>
2026-04-24 22:17:42 +0100

    Initial commit: Playwright Java Project de Testes com Playwright

---
+++ src/main/java/com/raghavpal/PlaywrightJavaProject/PlaywrightJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.raghavpal.PlaywrightJavaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaywrightJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaywrightJavaProjectApplication.class, args);
+	}
+
+}
